9e40be0d2d7b73c5af7fb502b4ef4f7326b0e291
Zain <96332173+zaindurrani@users.noreply.github.com>
2024-12-27 00:31:50 +0500

    Update datastructures.py

--- week2/datastructures.py
+++ week2/datastructures.py
@@ -5,13 +5,13 @@ list3 = [1,'c']
 print(list2[2])
 
 #practice
-thislist = ["cpple", "a     nana", "bherry"]
+thislist = ["apple", "banana", "bherry"]
 print(thislist[0:5])
 print(thislist[1])
-zainslist = ["macca","japan","dubai","new ghar make family stable","become server admin"]
+zainslist = ["c","b","a","1b","2a"]
 print(zainslist[2:5])
 print(zainslist[0:5])
-zainslist.remove("japan")
+zainslist.remove("c")
 thislist.sort()
 print(zainslist[0:5])
 # looping the list
